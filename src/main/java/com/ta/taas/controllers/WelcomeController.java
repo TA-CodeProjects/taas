efb8662b7904c81f747c9893432a03cfde7c8279
tomer <tomerab00@gmail.com>
2022-08-23 15:38:57 +0300

    controller map hello

--- src/main/java/com/ta/taas/controllers/WelcomeController.java
+++ src/main/java/com/ta/taas/controllers/WelcomeController.java
@@ -19,6 +19,11 @@ import java.util.UUID;
 public class WelcomeController {
     private final WelcomeService welcomeService;
 
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+
     @PostMapping("register")
     @ResponseStatus(HttpStatus.CREATED)
     public void register(@Valid @RequestBody RegisterReqDto registerReqDto) throws TaskSecurityException {
